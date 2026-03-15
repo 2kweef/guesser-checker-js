8986b9ddbabcb535b41e5ff054f13d51b675c109
2kweef <toufeekpro@outlook.com>
2026-03-15 20:38:41 +0000

    Completed

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const r1 = readline.createInterface({
 });
 
 let tries = 0;
-let guess = 0;
 
 const secret = Math.floor(Math.random() * 20) + 1;
+
+console.log('I am thinking of a number between 1 and 20.');
+
+function askGuess() {
+    r1.question('Enter your guess: ', function (input) {
+        const guess = Number(input);
+        tries = tries + 1;
+
+        if (guess === secret) {
+            console.log('Correct!');
+            r1.close();
+        } else {
+            console.log('Incorrect!');
+            askGuess();
+        }
+    });
+}
+
+askGuess();
